c41fa70c600fcc9550ef20e01202b714948f7a3f
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-04-27 20:58:39 +0100

    react-rotation

---
+++ rotation/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Rotation from 'react-rotation'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="otation">
+        <Rotation cycle={true}>
+          <img src='https://andrepolischuk.com/react-rotation/images/00.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/01.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/02.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/03.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/04.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/05.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/06.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/07.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/08.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/09.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/10.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/11.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/12.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/13.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/14.jpg' alt='camera' />
+          <img src='https://andrepolischuk.com/react-rotation/images/15.jpg' alt='camera' />
+        </Rotation>
+      </div>
+    </div>
+  );
+}
+
+export default App;
